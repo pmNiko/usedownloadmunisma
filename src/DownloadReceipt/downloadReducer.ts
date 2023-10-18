b9f970dd7d7fa9ccbf2306fab7fb6a331febb889
nikodev <martin.panebianco@smandes.gob.ar>
2023-10-18 09:06:16 -0300

    Version 1.0 estable

--- src/DownloadReceipt/downloadReducer.ts
+++ src/DownloadReceipt/downloadReducer.ts
@@ -1,33 +1,36 @@
-import { DownloadActionType, DownloadReducerState } from './interfaces'
+import { DownloadActionType, DownloadReducerState } from './interfaces';
 
 export const initialStateDownloadReducer: DownloadReducerState = {
-    token: '',
-    id: '',
-    zip: '',
-    receiptsDownload: { receipts: '', usuario: '' },
-    isFetching: false,
-    progress: 0,
-}
+  token: '',
+  id: '',
+  zip: '',
+  receiptsDownload: { receipts: '', usuario: '' },
+  isFetching: false,
+  progress: 0,
+};
 
-export const downloadReducer = (state: DownloadReducerState, action: DownloadActionType) => {
-    switch (action.type) {
-        case 'setToken':
-            return { ...state, token: action.payload }
-        case 'setId':
-            return { ...state, id: action.payload }
-        case 'setZip':
-            return { ...state, zip: action.payload }
-        case 'setReceiptsDownload':
-            return { ...state, receiptsDownload: action.payload }
-        case 'changeIsFetching':
-            return { ...state, isFetching: !state.isFetching }
-        case 'setProgress':
-            return { ...state, progress: action.payload }
+export const downloadReducer = (
+  state: DownloadReducerState,
+  action: DownloadActionType
+) => {
+  switch (action.type) {
+    case 'setToken':
+      return { ...state, token: action.payload };
+    case 'setId':
+      return { ...state, id: action.payload };
+    case 'setZip':
+      return { ...state, zip: action.payload };
+    case 'setReceipts':
+      return { ...state, receiptsDownload: action.payload };
+    case 'changeIsFetching':
+      return { ...state, isFetching: !state.isFetching };
+    case 'setProgress':
+      return { ...state, progress: action.payload };
 
-        case 'resetDownloadState':
-            return { ...initialStateDownloadReducer }
+    case 'reset':
+      return { ...initialStateDownloadReducer };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
